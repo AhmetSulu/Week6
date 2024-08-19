9a57f6ad812dd81c071def342cd7388d7420c095
Ahmet <Ahmet@DESKTOP-6Q0TJG5>
2024-08-19 12:49:41 +0300

    ekleme

---
+++ Week6/Homework12.sql
@@ -0,0 +1,25 @@
+﻿-- Film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+SELECT COUNT(*) FROM film
+WHERE length > (
+	SELECT AVG(length) FROM film
+);
+
+-- Film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+SELECT COUNT(*) FROM film
+WHERE rental_rate = (
+	SELECT MAX(rental_rate) FROM film
+);
+
+-- Film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+SELECT * FROM film
+WHERE 
+	rental_rate = (SELECT MIN(rental_rate) FROM film)
+	AND
+	replacement_cost = (SELECT MIN(replacement_cost) FROM film);
+
+-- Payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+SELECT customer.first_name, customer.last_name, COUNT(*) AS "shopping_count" FROM customer
+INNER JOIN payment
+ON customer.customer_id = payment.customer_id
+GROUP BY customer.customer_id
+ORDER BY COUNT(*) DESC;
